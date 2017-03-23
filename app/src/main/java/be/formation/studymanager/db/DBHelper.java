f62961caac4ec680085483c7b0a7b4895d35144c
MisterFsL <fsl.abouri2@gmail.com>
2017-03-23 12:22:49 +0100

    DB UPDATE

--- app/src/main/java/be/formation/studymanager/db/DBHelper.java
+++ app/src/main/java/be/formation/studymanager/db/DBHelper.java
@@ -20,11 +20,13 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(LessonDAO.CREATE_REQUEST);
+        db.execSQL(UserLessonDAO.CREATE_REQUEST);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL(LessonDAO.UPGRADE_REQUEST);
+
         onCreate(db);
     }
 }
